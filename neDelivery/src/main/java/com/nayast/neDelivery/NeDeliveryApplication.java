6425e7be5c7bb52076078e5ca1e783bbb82a10de
Nayane Amorim <nayaneast@gmail.com>
2024-03-20 21:28:22 -0300

    first commit

---
+++ neDelivery/src/main/java/com/nayast/neDelivery/NeDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.nayast.neDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeDeliveryApplication.class, args);
+	}
+
+}
---
+++ neDelivery/src/test/java/com/nayast/neDelivery/NeDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nayast.neDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
